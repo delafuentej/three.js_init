a358ab3ecf9e49843282bad2917207bdcd268ba7
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-17 20:07:54 +0200

    getting started

---
+++ drawingLines.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three';
+
+// if ( WebGL.isWebGL2Available() ) {
+
+// 	// Initiate function or other initializations here
+// 	animate();
+
+// } else {
+
+// 	const warning = WebGL.getWebGL2ErrorMessage();
+// 	document.getElementById( 'container' ).appendChild( warning );
+
+// }
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
+
+const scene = new THREE.Scene();
+
+//create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial( { color: 0x6a5acd} );
+
+
+//After material we will need a geometry with some vertices:
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+points.push( new THREE.Vector3( -10, 0, 0 ) );
+
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+//Now that we have points for two lines and a material, we can put them together to form a line.
+const line = new THREE.Line( geometry, material );
+
+//All that's left is to add it to the scene and call render.
+scene.add( line );
+renderer.render( scene, camera );
--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 import * as THREE from 'three';
+
+// To actually be able to display anything with three.js, we need three things: scene, camera and renderer
+
+// Setting up: 
+// 1. Scene
+const scene = new THREE.Scene();
+// 2. Camera
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// 3. Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+// To create a cube, we need a BoxGeometry. This is an object that contains all the points (vertices) and fill (faces) of the cube
+const geometry = new THREE.BoxGeometry( 1, 1,1 );
+// material:  Three.js comes with several materials but we'll stick to the MeshBasicMaterial for now
+const material = new THREE.MeshBasicMaterial( { color: 0xffa500} );
+//Mesh is an object that takes a geometry, and applies a material to it, which we then can insert to our scene, and move freely around.
+const cube = new THREE.Mesh( geometry, material );
+
+//the thing we add will be added to the coordinates (0,0,0)
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+
+    //animating the cube
+	cube.rotation.x += -0.03;
+	cube.rotation.y += 0.01;
+
+    // Rendering the scene
+	renderer.render( scene, camera );
+
+}
